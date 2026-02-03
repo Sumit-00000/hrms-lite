fca68b72b628633b8b9a1de309d7b4aac124c1ff
Sumit-00000 <146862886+Sumit-00000@users.noreply.github.com>
2026-02-03 16:22:32 +0530

    fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hrms-lite-qnf9.onrender.com"
+  baseURL: "https://hrms-lite-qnf9.onrender.com/api"
 });
 
 export const fetchEmployees = () => API.get("/employees");
