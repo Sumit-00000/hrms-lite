ed16401b756dbc4d8c7189b0be20c30283af97b8
Sumit-00000 <146862886+Sumit-00000@users.noreply.github.com>
2026-02-03 16:18:08 +0530

    Update frontend to use live backend API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://hrms-lite-qnf9.onrender.com"
 });
 
 export const fetchEmployees = () => API.get("/employees");
